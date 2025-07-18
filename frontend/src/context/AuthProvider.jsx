85fed8b1f799e171c6bcdd1cb79b057fc9abad7c
Aman Chaurasiya <139747366+aman8906@users.noreply.github.com>
2025-07-18 16:09:08 +0530

    Update AuthProvider.jsx

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -16,7 +16,7 @@ export const AuthProvider = ({ children }) => {
         console.log(token);
         if (token) {
           const { data } = await axios.get(
-            "http://localhost:4001/api/users/my-profile",
+            `${import.meta.env.VITE_API_BASE_URL}/api/users/my-profile`,
             {
               withCredentials: true,
               headers: {
