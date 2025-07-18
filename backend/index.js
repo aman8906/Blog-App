a31ee01ca54ad87c94db84eda2202452d5bafc56
Aman Chaurasiya <ftpchaursiyaaman@gmail.com>
2025-07-18 14:53:07 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL.split(","),
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
