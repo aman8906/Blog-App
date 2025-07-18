031c6c8cf0f489f22dbb603cc86806be491b159c
Aman Chaurasiya <139747366+aman8906@users.noreply.github.com>
2025-07-18 16:20:38 +0530

    Update UpdateBlog.jsx

--- frontend/src/dashboard/UpdateBlog.jsx
+++ frontend/src/dashboard/UpdateBlog.jsx
@@ -29,7 +29,7 @@ function UpdateBlog() {
     const fetchBlog = async () => {
       try {
         const { data } = await axios.get(
-          `http://localhost:4001/api/blogs/single-blog/${id}`,
+          `${import.meta.env.VITE_API_BASE_URL}/api/blogs/single-blog/${id}`,
 
           {
             withCredentials: true,
