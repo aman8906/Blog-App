48caaf662ca5b9ba3306771043e43f074015aa32
Aman Chaurasiya <139747366+aman8906@users.noreply.github.com>
2025-07-18 16:12:14 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
     e.preventDefault();
     try {
       const { data } = await axios.get(
-        "http://localhost:4001/api/users/logout",
+       `${import.meta.env.VITE_API_BASE_URL}/api/users/logout`, 
         { withCredentials: true }
       );
       console.log(data);
