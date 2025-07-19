4a18bd1eac54dfe6ec50c57ad2f82108f3cdf1fe
Aman Chaurasiya <139747366+aman8906@users.noreply.github.com>
2025-07-19 10:01:43 +0530

    Update MyBlogs.jsx

--- frontend/src/dashboard/MyBlogs.jsx
+++ frontend/src/dashboard/MyBlogs.jsx
@@ -36,7 +36,7 @@ function MyBlogs() {
   };
   return (
     <div>
-      <div className="container w-full flex justify-center my-10 px-2">
+      <div className="container w-full flex justify-center my-18 px-8">
         <div className="grid gap-6 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 md:ml-20">
           {myBlogs && myBlogs.length > 0 ? (
             myBlogs.map((element) => (
